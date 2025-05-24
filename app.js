f0d6f4b45f89496d7ff51424a7271476a338cdff
Sonny <Sonnyhimawan13456@gmail.com>
2025-05-24 15:45:07 +0700

    Membuat active todo dan initial untuk seleksi data dari masing masing tab

--- app.js
+++ app.js
@@ -26,50 +26,37 @@ setInterval(myDateNow, 1000);
 
 
 
-// My Todo 
-
-
-let myTodolist = [];
-let id = 0;
-
-
-// Get Element 
-
+let myTodolist = JSON.parse(localStorage.getItem("itemTodo")) || [];
+let id = myTodolist.length > 0 ? myTodolist[myTodolist.length - 1].id + 1 : 1;
 const myForm = document.getElementById("todo-form");
-
-
-// End GET Element
-
 
 //  Function Add Todo
 
 const addTodo = () => {
 
+
   myForm.addEventListener("submit", (e) => {
     e.preventDefault();
- 
-    data = {
-    id : id++, 
-    priority : document.getElementById("input-priority").value,
-    date : document.getElementById("input-date").value,
-    task : document.getElementById("input-todo").value.trim(),
-    status : false
+    itemTodo = {
+      id: id++,
+      priority: document.getElementById("input-priority").value,
+      date: document.getElementById("input-date").value,
+      task: document.getElementById("input-todo").value.trim(),
+      status: false
     };
 
-    myTodolist.push(data);
-
-    myForm.reset();
-   
-
-
-
-    if (data.priority === "" || data.date === "" || data.task === "") {
-     alert("Silahkan isi seluruh form");
+    if (itemTodo.priority === "" || itemTodo.date === "" || itemTodo.task === "") {
+      alert("Silahkan isi seluruh form");
+      return;
     } else {
-      alert("Data Berhasil Ditambahkan");
-      console.log(myTodolist);
-      localStorage.setItem("data",JSON.stringify(myTodolist));
+      alert("Data Todolist Berhasil Ditambahkan");
     }
+
+    myTodolist.push(itemTodo);
+    saveTodo();
+    myForm.reset();
+    renderTodo();
+
   });
 
 }
@@ -79,20 +66,49 @@ addTodo();
 
 // End Function Add Todo
 
+const saveTodo = () => {
+  localStorage.setItem("itemTodo", JSON.stringify(myTodolist));
+}
 
 
 
 
+const activeTodo = () => {
+
+  const myTab = document.getElementById("tab");
+  myTab.addEventListener("click", (e) => {
+
+    const tabActive = e.target.closest("[data-tab]");
+    if (!tabActive) return;
+    const tabName = tabActive.dataset.tab;
+    myTab.querySelectorAll('[data-tab]').forEach(e => {
+      e.classList.remove('text-lg', 'border-b-2', 'text-blue-600', 'font-medium');
+    });
+    tabActive.classList.add('cursor-pointer', 'border-b-2', 'text-lg', 'font-medium', 'text-blue-600', 'inline-block', '-mb[1px]');
+    showTabContent(tabName);
+  });
+
+}
+
+activeTodo();
 
 
+const showTabContent = (tabActive) => {
 
+  const myContent = {
+    todo: document.getElementById("list-todo"),
+    done: document.getElementById("list-done"),
+    overdue: document.getElementById("list-overdue")
+  }
 
-
-
-
-
-
-
+  for (const key in myContent) {
+    if (key === tabActive) {
+      myContent[key].classList.remove("hidden");
+    } else {
+      myContent[key].classList.add("hidden");
+    }
+  }
+};
 
 
 
